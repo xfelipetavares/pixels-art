4b3803b52fe9a2be3effecbbf1006d0dff3744ae
Felipe Tavares <xfelipetavares@gmail.com>
2022-10-24 09:40:28 -0300

    implementado board de pixel dinamica com mudança com confirmação direta do input

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 const boardPixel = document.getElementsByClassName(`board-pixel`)[0];
+const inputSizeBoard = document.getElementById(`input-size-board`);
+const buttonPlus = document.getElementById(`button-plus`);
+const buttonSub = document.getElementById(`button-sub`);
 
 const boardCreator = (num) => {
   for (let index = 0; index < num * num; index++) {
@@ -9,4 +12,16 @@ const boardCreator = (num) => {
   boardPixel.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
   boardPixel.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 };
-boardCreator(25);
+const boardClear = (num) => {
+  while (boardPixel.children.length != 0) {
+    const childBoard = boardPixel.firstElementChild;
+    boardPixel.removeChild(childBoard);
+  }
+  return boardCreator(num);
+};
+window.onload = () => {
+  boardCreator(inputSizeBoard.value);
+  inputSizeBoard.addEventListener(`input`, () => {
+    boardClear(inputSizeBoard.value);
+  });
+};
